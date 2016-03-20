389c63ceb8309d669556be5cc5456f715d4d8e8d
Yves Talboom <yves.talboom@gmail.com>
2016-03-20 14:33:55 +0100

    Add year validation

--- public/scripts/controller/registerStudentController.js
+++ public/scripts/controller/registerStudentController.js
@@ -8,6 +8,13 @@
 
     function RegisterStudentController($state, Data) {
         var vm = this;
+
+        vm.yearChanged = function () {
+            var year = parseInt(vm.formData.studentYear);
+            if(year) {
+                vm.yearError = year > 2016 || year < 1970;
+            }
+        };
         
         vm.processForm = function() {
             
