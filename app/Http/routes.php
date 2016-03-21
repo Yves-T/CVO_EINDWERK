cfb1b98973f5d10198f451a9ee2b9a05cba0aa78
Yves Talboom <yves.talboom@gmail.com>
2016-03-21 17:25:15 +0100

    Add 404 page

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,7 +2,7 @@
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('index');
 
 Route::group(['prefix' => 'api'], function () {
     Route::resource('authenticate', 'AuthenticateController', ['only' => ['index']]);
