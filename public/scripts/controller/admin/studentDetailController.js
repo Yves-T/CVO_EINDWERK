3fe928d5cbe0e21e20a952134c1c8d64c293ce5e
Yves Talboom <yves.talboom@gmail.com>
2016-03-24 12:24:33 +0100

    Prevent page reload "forget" the actual student

--- public/scripts/controller/admin/studentDetailController.js
+++ public/scripts/controller/admin/studentDetailController.js
@@ -22,13 +22,25 @@
 
         var vm = this;
         vm.student = $stateParams.student;
+
+        if (!vm.student) {
+            var student = JSON.parse(sessionStorage.getItem('studentDetail'));
+            if (student) {
+                vm.student = student;
+            }
+        }
+
         if (vm.student) {
+            var studentAsString = JSON.stringify(vm.student);
+            sessionStorage.setItem('studentDetail', studentAsString);
+
             moment.locale('nl', null);
             var createdAt = moment(vm.student.created_at);
             vm.createdAt = createdAt.locale('nl').format("LLL");
         }
 
         vm.goBack = function () {
+            sessionStorage.removeItem('studentDetail');
             $state.go('adminManageUsers', {
                 "message": null,
                 "error": null
